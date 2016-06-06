9551383cefb5a6636cb9ccf7f72869293e83f25c
Adrian Kalbarczyk <adrian.kalbarczyk@gmail.com>
2016-06-07 00:38:46 +0200

    ddd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-s3cmd sync -r css img js index.html s3://gluu.io/
+s3cmd sync -r css img js index.html privacy.html s3://gluu.io/
 #s3cmd setacl s3://acf.acimg.eu/css/ --acl-public --recursive
 s3cmd setacl s3://gluu.io/ --acl-public --recursive
